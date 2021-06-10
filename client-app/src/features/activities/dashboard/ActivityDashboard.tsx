e9754fc8e7bd86d2a6d3a20d3f025b90e84cb502
Vadim Zylevich <vzy@leapwork.com>
2021-06-10 23:35:07 +0200

    Dirty

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,12 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (acitivity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, deleteActivity,
-        selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit }: Props) {
+        selectActivity, cancelSelectActivity, editMode, openForm, 
+        closeForm, createOrEdit, submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -37,7 +39,12 @@ export default function ActivityDashboard({activities, selectedActivity, deleteA
                     
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting} 
+                />}
             </Grid.Column>
         </Grid>
     )
